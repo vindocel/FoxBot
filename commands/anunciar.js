3fe13c3e526238ef9e6e38cd9dca143890cf4bfc
Victor Matos <victor239tito@hotmail.com>
2020-03-06 13:55:05 -0400

    ajuste
    apagar msg de alerta dps de um tempo

--- commands/anunciar.js
+++ commands/anunciar.js
@@ -11,7 +11,7 @@ module.exports = {
 
     //boloquio por cargo
     if(!message.member.roles.some(r=>["652592408550178827", "654667200124289034"].includes(r.id)))
-        return message.reply(`<:stop:653479507650674729> Você Não tem Permissao para Usar este Comando, mais caso queria usar entre aqui <#652592442494812171>`);
+        return message.reply(`<:stop:653479507650674729> Você Não tem Permissao para Usar este Comando, mais caso queria usar entre aqui <#652592442494812171>`).then(d_msg => { d_msg.delete(9000);});
 
         // detectar canal /imagem
 
@@ -26,12 +26,12 @@ module.exports = {
         //fim
      
         if (match === null) {
-            message.channel.send("<a:sino:654177101322256395> Você precisa adicionar uma <:image:653462486544023552>`IMAGEM` valida que tenha o final `.jpg .gif .png`")
+            message.channel.send("<a:sino:654177101322256395> Você precisa adicionar uma <:image:653462486544023552>`IMAGEM` valida que tenha o final `.jpg .gif .png`").then(d_msg => { d_msg.delete(9000);});
             return
         }
 
         if (!canalMencionado) {
-         message.channel.send("<:stop:653479507650674729> Você precisa mencionar sua loja ex `#loja-victor`")
+         message.channel.send("<:stop:653479507650674729> Você precisa mencionar sua loja ex `#loja-victor`").then(d_msg => { d_msg.delete(9000);});
             return
         }
         const reasonFiltrada = reason.replace(/\<\#\d{18}\>/gi, "")
