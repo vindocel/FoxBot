7b778dcfab47be27cb5f126c1dec238593d05c49
VictorTG123456 <61212993+VictorTG123456@users.noreply.github.com>
2020-08-16 00:29:14 -0300

    0:28 16/08/2020
    Ajudante pode dar warn

--- commands/warn.js
+++ commands/warn.js
@@ -12,7 +12,7 @@ module.exports = {
     run: async (client, message, args) => {
     if (message.channel.type === "dm") return;
 
-    if(!message.member.roles.some(r=>["652592408550178827", "652592409582239744", "652592410345472050", "652592411003846697"].includes(r.id)))
+    if(!message.member.roles.some(r=>["652592408550178827", "652592409582239744", "652592410345472050", "652592413420027926", "652592411003846697"].includes(r.id)))
         return message.reply(`<:stop:653479507650674729> Você Não tem Permissao para Usar este Comando, <:cancelar:653462167076470785>`);
 
     if (message.mentions.members.size === 0)
