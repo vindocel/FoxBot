2817a995ce48f6d0816391f2dcdb79a368179c4e
VictorTG123456 <61212993+VictorTG123456@users.noreply.github.com>
2020-08-21 11:46:59 -0300

    11:46 21/08/2020
    Colocando descrição

--- commands/lojavip.js
+++ commands/lojavip.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 module.exports = {
-    name: "",
-    description: "",
+    name: "lojavip",
+    description: "comando lojavip",
     run: async (client, message, args) => {
 
     if (message.channel.type === "dm") return;
